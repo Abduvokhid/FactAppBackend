e171566b6ca528a44a9350dd47ce68f5e1b37905
Abduvokhid <Abduvohid1996@inbox.ru>
2022-01-08 18:36:57 +0500

    Added privacy policy

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ router.use('/', require('./dashboard'))
 router.use('/api', require('./api'))
 router.use('/categories', require('./categories'))
 router.use('/facts', require('./facts'))
+router.get('/privacy', (req, res) => res.render('privacy', { layout: false }))
 
 module.exports = router
