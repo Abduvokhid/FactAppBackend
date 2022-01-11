02701d572c7e31bfb72e0c0942e45042da795dc5
Abduvokhid <Abduvohid1996@inbox.ru>
2022-01-11 17:37:39 +0500

    Fixing a bug

--- DAL/fact.js
+++ DAL/fact.js
@@ -1,11 +1,11 @@
 const Fact = require('../models/Fact')
 
 module.exports.findAllFacts = () => {
-  return Fact.find().populate('category').sort('-created_date').lean()
+  return Fact.find().populate('category').sort('created_date').lean()
 }
 
 module.exports.findFactsAfterDate = (date) => {
-  return Fact.find({ created_date: { $gt: date } }).populate('category').sort('-created_date').lean()
+  return Fact.find({ created_date: { $gt: date } }).populate('category').sort('created_date').lean()
 }
 
 module.exports.findFactByCategoryID = (category_id) => {
