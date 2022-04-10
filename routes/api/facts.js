194c9c8a42298a97b91447c3cd06557d4e948de2
Abduvokhid <abduvohid1996@inbox.ru>
2022-04-11 00:37:13 +0500

    Added limit and offset to get facts method

--- routes/api/facts.js
+++ routes/api/facts.js
@@ -2,10 +2,13 @@ const router = require('express').Router()
 const { Fact } = require('../../DAL')
 
 router.get('/', async (req, res) => {
+  const { offset = 0, limit = 50 } = req.query
   const facts = await Fact.findAll({
     include: {
       association: 'categories',
       through: { attributes: [] },
+      where: { id: { $gt: offset } },
+      limit: limit,
     }
   })
   res.json({
