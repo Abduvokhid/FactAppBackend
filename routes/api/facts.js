83c4207b344ad106bb85070b82d018cde485eb4c
Abduvokhid <abduvohid1996@inbox.ru>
2022-04-11 00:43:31 +0500

    Added limit and offset to get facts method

--- routes/api/facts.js
+++ routes/api/facts.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const { Fact } = require('../../DAL')
+const { Op } = require('sequelize')
 
 router.get('/', async (req, res) => {
   const { offset = 0, limit = 50 } = req.query
   const facts = await Fact.findAll({
-    where: { id: { $gt: offset } },
+    where: { id: { [Op.gt]: offset } },
     limit: limit,
     include: {
       association: 'categories',
